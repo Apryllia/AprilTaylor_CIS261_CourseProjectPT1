360aeb61700e10629ecf3c26616fdb3f7fb43f56
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-07-29 16:37:04 -0700

    changing code

--- AprilTaylor_CIS261_CourseProjectPT1.py
+++ AprilTaylor_CIS261_CourseProjectPT1.py
@@ -1,39 +1,21 @@
 def GetEmpInfo():
-    EmpName = input("Enter Employee Name:")
-    hours = int(input("Enter Number of Hours Worked:"))
-    PayRate = float(input("Enter Hourly Pay Rate:"))
-    return EmpName, hours, PayRate
+    EmpName = input("Enter Employee Name: ")
+    return EmpName
 
-def CalcWklyPay(hours, PayRate):
-    return hours * PayRate
+def GetHoursWorked():
+    hours = float(input("Enter Hours Worked: "))
+    return hours
 
-def IncomeTaxRate(WeeklyPay):
-    ITR = 10
-    if WeeklyPay >= 1000:
-       ITR = 12
-    elif WeeklyPay >= 2000:
-       ITR = 22
-    elif WeeklyPay < 1000:
-        ITR = 10
-    return ITR
+def GetHourlyRate():
+    hourrate = float(input("Enter Hourly Rate: "))
+    return hourrate
 
-if __name__== "__main__":
-    #main()
-    EmpName, hours, PayRate = GetEmpInfo()
-    print(EmpName)
-    print(hours)
-    print("$" + format(PayRate, ",.2f"))
-    WKP = CalcWklyPay(hours, PayRate)
-    print("$" + format(WKP, ",.2f"))
-    print("$" + format(IncomeTaxRate(WKP)))
+def GetTaxRate():
+    taxrate = float(input("Enter tax rate: "))
+    return taxrate
 
-def IncomeTaxRate(WeeklyPay):
-    ITR = 10
-    if WeeklyPay >= 1000:
-       ITR = 12
-    elif WeeklyPay >= 2000:
-       ITR = 22
-    elif WeeklyPay < 1000:
-        ITR = 10
-    return ITR
-
+def CalcTaxNetPay(hours, hourrate, taxrate):
+    grosspay = hours * hourrate
+    incometax = grosspay * taxrate
+    netpay = grosspay - incometax
+    return grosspay, incometax, netpay
