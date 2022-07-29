70882554525c24710d98d38ae3589e0a90f04704
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-07-29 15:33:41 -0700

    added code

--- AprilTaylor_CIS261_CourseProjectPT1.py
+++ AprilTaylor_CIS261_CourseProjectPT1.py
@@ -7,10 +7,33 @@ def GetEmpInfo():
 def CalcWklyPay(hours, PayRate):
     return hours * PayRate
 
+def IncomeTaxRate(WeeklyPay):
+    ITR = 10
+    if WeeklyPay >= 1000:
+       ITR = 12
+    elif WeeklyPay >= 2000:
+       ITR = 22
+    elif WeeklyPay < 1000:
+        ITR = 10
+    return ITR
+
 if __name__== "__main__":
     #main()
     EmpName, hours, PayRate = GetEmpInfo()
     print(EmpName)
     print(hours)
-    print(PayRate)
-    print(CalcWklyPay(hours, PayRate))
+    print("$" + format(PayRate, ",.2f"))
+    WKP = CalcWklyPay(hours, PayRate)
+    print("$" + format(WKP, ",.2f"))
+    print("$" + format(IncomeTaxRate(WKP)))
+
+def IncomeTaxRate(WeeklyPay):
+    ITR = 10
+    if WeeklyPay >= 1000:
+       ITR = 12
+    elif WeeklyPay >= 2000:
+       ITR = 22
+    elif WeeklyPay < 1000:
+        ITR = 10
+    return ITR
+
