489f1b58c0e0c138869784c85e4577dc81b5482b
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-07-30 14:05:55 -0700

    new

--- AprilTaylor_CIS261_CourseProjectPT1.py
+++ AprilTaylor_CIS261_CourseProjectPT1.py
@@ -1,21 +1,60 @@
-def GetEmpInfo():
-    EmpName = input("Enter Employee Name: ")
-    return EmpName
+keepGoing = "y"
+while keepGoing.lower()== "y":
 
-def GetHoursWorked():
-    hours = float(input("Enter Hours Worked: "))
-    return hours
+    def GetEmpInfo():
+        EmpName = input("Enter Employee Name: ")
+        return EmpName
 
-def GetHourlyRate():
-    hourrate = float(input("Enter Hourly Rate: "))
-    return hourrate
+    def GetHoursWorked():
+        hours = float(input("Enter Hours Worked: "))
+        return hours
 
-def GetTaxRate():
-    taxrate = float(input("Enter tax rate: "))
-    return taxrate
+    def GetHourlyRate():
+        hourrate = float(input("Enter Hourly Rate: "))
+        return hourrate
+
+    def GetTaxRate():
+        taxrate = float(input("Enter tax rate: "))
+        return taxrate
+
+keepGoing = input("\nThis show is over but you can always do it again. Do you wanna go to a different show? Enter yes or no:")
 
 def CalcTaxNetPay(hours, hourrate, taxrate):
     grosspay = hours * hourrate
     incometax = grosspay * taxrate
     netpay = grosspay - incometax
     return grosspay, incometax, netpay
+
+def printinfo(EmpName, hours, hourrate, grosspay, taxrate, incometax, netpay ):
+    print(EmpName, f"{hours:,.2f}", f"{grosspay:,.2f}", f"{taxrate:,.2%}", f"{incometax:,.2f}", f"{netpay:,.2f}")
+
+def PrintTotals(TotEmp, TotHours, TotGp, TotTax, TotNp):
+    print()
+    print(f"Total Number of Employees: {TotEmp}")
+    print(f"Total Hours Work: {TotHours:,.2f}")
+    print(f"Total Gross Pay: {TotGp:,.2f}")
+    print(f"Total Income Tax: {TotTax:,.2f}")
+    print(f"Total Net Pay: {TotNp:,.2f}")
+
+if __name__== "__main__":
+    TotEmp = 0
+    TotHours = 0.00
+    TotGp = 0.00
+    TotTax = 0.00
+    TotNp = 0.00
+    while True:
+        EmpName = GetEmpInfo()
+        if (EmpName.upper() == "End"):
+            break
+        hours = GetHoursWorked()
+        hourrate = GetHourlyRate()
+        taxrate = GetTaxRate()
+        grosspay, incometax, netpay = CalcTaxNetPay(hours, hourrate, taxrate)
+        printinfo(EmpName, hours, hourrate, grosspay, taxrate, incometax, netpay)
+        TotEmp += 1
+        TotHours += hours
+        TotGp += grosspay
+        TotTax += incometax
+        TotNp += netpay
+        
+    PrintTotals (TotEmp, TotHours, TotGP, TotTax, TotNp)
